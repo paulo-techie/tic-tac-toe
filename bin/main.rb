2990396e831d8ea94d7d1e923ec78e7d783001fa
Ricardo <ricardotvera.54@gmail.com>
2020-05-21 09:00:23 -0700

    Update main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,26 @@
 #!/usr/bin/env ruby
-puts "Hello World!"
+# frozen_string_literal: true
+
+require_relative '../lib/player'
+
+class Tictactoe
+  attr_accessor :board, :players
+
+  def initialize
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    @players = []
+  end
+
+  def start
+    puts "enter player 1 name"
+    @players.push(gets.chomp)
+    puts "enter player 2 name"
+    @players.push(gets.chomp)
+  end
+
+  def show_board
+    puts "#{@board[0]} #{@board[1]} #{@board[3]}"
+    puts "#{@board[4]} #{@board[5]} #{@board[6]}"
+    puts "#{@board[7]} #{@board[8]} #{@board[9]}"
+  end
+end
