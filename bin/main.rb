8f28ce4c488c3d88783a5d50119c67bf5aaff729
Ricardo <ricardotvera.54@gmail.com>
2020-05-21 09:41:27 -0700

    Update main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -4,11 +4,16 @@
 require_relative '../lib/player'
 
 class Tictactoe
-  attr_accessor :board, :players
+  attr_accessor :board, :players, :turn, :p1_move, :p2_move
 
   def initialize
-    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     @players = []
+    @turn = 1 
+    @p1_move = []
+    @p2_move = []
+    @move = ""
+    @used = []
   end
 
   def start
@@ -19,8 +24,45 @@ class Tictactoe
   end
 
   def show_board
-    puts "#{@board[0]} #{@board[1]} #{@board[3]}"
-    puts "#{@board[4]} #{@board[5]} #{@board[6]}"
-    puts "#{@board[7]} #{@board[8]} #{@board[9]}"
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts "---------"
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts "---------"
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
+
+  def player_turn
+    if @turn.even? 
+    puts "this is #{@players[1]} turn"
+    else
+    puts "this is #{@players[0]} turn"
+    end
+  end
+ 
+    def action 
+    if @turn.even? 
+      puts "please #{@players[1]} enter a number between 1-9"
+      @move = gets.chomp
+      if @used.include?(@move)  
+        puts "that spot is already taken"
+      elsif (@move.respond_to?(:to_i) == false) || @move.to_i > 9 || @move.to_i < 1 
+        puts "thats and invalid answer"
+      else
+        @used.push(@move.to_i)
+        @p2_move.push(@move.to_i)
+        @turn += 1
+      end
+      else
+      puts "please #{@players[0]} enter a number between 1-9"
+      @move = gets.chomp
+      if @used.include?(@move)  
+        puts "that spot is already taken"
+      elsif (@move.respond_to?(:to_i) == false) || @move.to_i > 9 || @move.to_i < 1 
+        puts "thats and invalid answer"
+      else
+        @used.push(@move.to_i)
+        @p1_move.push(@move.to_i)
+        @turn += 1
+      end
+    end
 end
