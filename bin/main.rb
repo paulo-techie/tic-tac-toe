b6613441c85531d5df6180358f48b870bef56a74
Ricardo <ricardotvera.54@gmail.com>
2020-05-27 00:11:34 -0700

    Update main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -4,37 +4,37 @@
 require_relative '../lib/logic.rb'
 
 class Game < Tictactoe
-  
+
   def start
-    puts "************************************************************"
-    puts "*                Please enter player 1 name                *"
-    puts "************************************************************"
+    puts '************************************************************'
+    puts '*                Please enter player 1 name                *'
+    puts '************************************************************'
     @players.push(gets.chomp)
-    puts "************************************************************"
-    puts "*                Please enter player 2 name                *"
-    puts "************************************************************"
+    puts '************************************************************'
+    puts '*                Please enter player 2 name                *'
+    puts '************************************************************'
     @players.push(gets.chomp)
   end
 
   def show_board
-    puts "************************************************************"
-    puts "                         ___________                                     "
-    puts "                        |           |                                     "  
-    puts "                        | #{@board[0]} | #{@board[1]} | #{@board[2]} |         "
-    puts "                        | --------- |                          "
-    puts "                        | #{@board[3]} | #{@board[4]} | #{@board[5]} |         "
-    puts "                        | --------- |                         "
-    puts "                        | #{@board[6]} | #{@board[7]} | #{@board[8]} |         "
-    puts "                        |___________|                                     "
-    puts ""
-    puts "************************************************************"
-end
+    puts '************************************************************'
+    puts '                         ___________                                     '
+    puts '                        |           |                                     '  
+    puts '                        | #{@board[0]} | #{@board[1]} | #{@board[2]} |         '
+    puts '                        | --------- |                          '
+    puts '                        | #{@board[3]} | #{@board[4]} | #{@board[5]} |         '
+    puts '                        | --------- |                         '
+    puts '                        | #{@board[6]} | #{@board[7]} | #{@board[8]} |         '
+    puts '                        |___________|                                     '
+    puts ''
+    puts '************************************************************'
+  end
 
   def player_turn
     if @turn.even? 
-    puts "this is #{@players[1]} turn"
+    puts 'this is #{@players[1]} turn'
     else
-    puts "this is #{@players[0]} turn"
+    puts 'this is #{@players[0]} turn'
     end
   end
  
@@ -42,58 +42,58 @@ end
     if @turn.even? 
       @check = @turn
       while @check == @turn
-        puts "************************************************************"
-        puts "*     please #{@players[1]} enter a number between 1-9     *"
-        puts "************************************************************"
+        puts '************************************************************'
+        puts '*     please #{@players[1]} enter a number between 1-9     *'
+        puts '************************************************************'
       @move = gets.chomp
       if @used.include?(@move.to_i)  
-        puts "that spot is already taken"
+        puts 'that spot is already taken'
       elsif (@move.respond_to?(:to_i) == false) || @move.to_i > 9 || @move.to_i < 1 
-        puts "thats and invalid answer"
+        puts 'thats and invalid answer'
       else
         @used.push(@move.to_i)
         @p2_move.push(@move.to_i)
         @turn += 1
-        @board[@move.to_i - 1] =  "O"
+        @board[@move.to_i - 1] =  'O'
       end
     end
       else
       @check = @turn
       while @check == @turn
-        puts "************************************************************"
-        puts "*     please #{@players[0]} enter a number between 1-9     *"
-        puts "************************************************************"
+        puts '************************************************************'
+        puts '*     please #{@players[0]} enter a number between 1-9     *'
+        puts '************************************************************'
       @move = gets.chomp
       if @used.include?(@move.to_i)
-        puts "************************************************************"
-        puts "*               that spot is already taken                 *"
-        puts "************************************************************"  
+        puts '************************************************************'
+        puts '*               that spot is already taken                 *'
+        puts '************************************************************'
       elsif (@move.respond_to?(:to_i) == false) || @move.to_i > 9 || @move.to_i < 1
-        puts "************************************************************"
-        puts "*                thats and invalid answer                  *"
-        puts "************************************************************"  
+        puts '************************************************************'
+        puts '*                thats and invalid answer                  *'
+        puts '************************************************************'  
       else
         @used.push(@move.to_i)
         @p1_move.push(@move.to_i)
         @turn += 1
-        @board[@move.to_i - 1] =  "X"
+        @board[@move.to_i - 1] =  'X'
       end
     end
     end
   end
   def look
     if @win1 == true
-      puts "************************************************************"
-      puts "*                player 1 is the winner                    *"
-      puts "************************************************************" 
+      puts '************************************************************'
+      puts '*                player 1 is the winner                    *'
+      puts '************************************************************' 
     elsif @win2 == true
-      puts "************************************************************"
-      puts "*                player 2 is the winner                    *"
-      puts "************************************************************"  
+      puts '************************************************************'
+      puts '*                player 2 is the winner                    *'
+      puts '************************************************************'  
     elsif $draw == true
-      puts "************************************************************"
-      puts "*                   this is a nice Draw                    *"
-      puts "************************************************************" 
+      puts '************************************************************'
+      puts '*                   this is a nice Draw                    *'
+      puts '************************************************************' 
     end  
   end  
 end
