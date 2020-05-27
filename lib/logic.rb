b9f281cbe60e7ace925a8daa291e6a97c3ef9811
Ricardo <ricardotvera.54@gmail.com>
2020-05-27 00:01:09 -0700

    Update logic.rb

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,31 +1,31 @@
 require_relative './player.rb'
 class Tictactoe
   attr_reader :players, :board, :turn, :p1_move, :p2_move, :win1, :win2, :answer
-  
+
   def initialize
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
     @players = []
-    @turn = 1 
+    @turn = 1
     @p1_move = []
     @p2_move = []
-    @move = ""
+    @move = ''
     @used = []
     @win1 = false
     @win2 = false
     win = false
     draw = false
     @condition = [[1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 5, 7], [3, 6, 9], [4, 5, 6], [7, 8, 9]]
-    @answer = ""
+    @answer = ''
     @repeat = true
   end
 
   def check
     if @condition.include?(@p1_move.sort)
       @win1 = true
-      $win = true
-      elsif @condition.include?(@p2_move.sort)
+      win = true
+    elsif @condition.include?(@p2_move.sort)
       @win  = true
-      $win = true
+      win = true
     end
     if @turn > 9 
       draw = true
