9a9dd323a3a5ad3a1cea5c0fff570cee4e903384
paulo-techie <paulo.techie@gmail.com>
2020-06-09 15:19:41 -0600

    Add tests for Game class: start, show_board

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper.rb'
 require './lib/logic.rb'
+require './lib/player.rb'
+require './bin/main.rb'
 
 describe Tictactoe do
     let(:tictactoe) {Tictactoe.new}
@@ -36,6 +38,43 @@ describe Tictactoe do
             expect(tictactoe.turn>9).to eql(false)
         end
     end
+end
 
+describe Player do
+    let(:player) {Player.new("doe", [])}
+    
+    describe 'initialize' do
+        it 'initialize player name' do
+            expect(player.name).to eql("doe")
+        end
 
+        it 'initialize player move with array' do
+            expect(player.move).to be_a(Array)
+        end
+    end
+end
+
+describe Game do
+    let(:game) {Game.new}
+    # let(:turn1) {4}
+    # let(:turn2) {5}
+
+    describe 'start' do
+        it 'should receive player names' do
+            expect(game.start).to be_a(Player)
+        end
+    end
+
+    describe 'show_board' do
+        it 'print a string representing the board layout' do
+            expect(game.show_board).to be(nil)
+        end
+    end
+
+    describe 'player_turn' do
+        it 'return string showing the player whose turn it is' do
+            expect(game.player_turn).to be_instance_of(NilClass)
+        end
+    end
+    
 end
